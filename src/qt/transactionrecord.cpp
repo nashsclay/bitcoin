050c4e32b5b6452650a694d9379a028dc8a3b1f5
John Studnicka <ComputerCraftr@users.noreply.github.com>
2020-12-11 04:19:10 -0700

    Fix coinstake TX in GUI

--- src/qt/transactionrecord.cpp
+++ src/qt/transactionrecord.cpp
@@ -35,7 +35,20 @@ QList<TransactionRecord> TransactionRecord::decomposeTransaction(const interface
     uint256 hash = wtx.tx->GetHash();
     std::map<std::string, std::string> mapValue = wtx.value_map;
 
-    if (nNet > 0 || wtx.is_coinbase)
+    if (wtx.is_coinstake) // peercoin: coinstake transaction
+    {
+        TransactionRecord sub(hash, nTime, TransactionRecord::StakeMint, "", -nDebit, wtx.tx->GetValueOut());
+        CTxDestination address;
+        const CTxOut& txout = wtx.tx->vout[1];
+        isminetype mine = wtx.txout_is_mine[1];
+
+        if(ExtractDestination(txout.scriptPubKey, address) && wtx.txout_address_is_mine[1])
+            sub.address = EncodeDestination(address);
+
+        sub.involvesWatchAddress = mine & ISMINE_WATCH_ONLY;
+        parts.append(sub);
+    }
+    else if (nNet > 0 || wtx.is_coinbase)
     {
         //
         // Credit
@@ -188,7 +201,7 @@ void TransactionRecord::updateStatus(const interfaces::WalletTxStatus& wtx, cons
         }
     }
     // For generated transactions, determine maturity
-    else if(type == TransactionRecord::Generated)
+    else if(type == TransactionRecord::Generated || type == TransactionRecord::StakeMint)
     {
         if (wtx.blocks_to_maturity > 0)
         {
